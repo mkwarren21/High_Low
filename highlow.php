73036febb21fca270985c365963fd745e9457902
Mary Warren <Friday@Marys-MacBook-Air.local>
2015-11-04 23:59:42 -0600

    added parameter feature. also added a check if someone gets it correct on th first try.

--- highlow.php
+++ highlow.php
@@ -1,24 +1,38 @@
 <?php
 
-$number = mt_rand(1,100);
-$a = 0;
 
-fwrite(STDOUT, 'Pick a number. Any number. Well, between 1 and 100.');
+if (($argc == 3) && (is_numeric($argv[1])) && (is_numeric($argv[2]))){
+	$max = $argv[2];
+	$min = $argv[1];
+} else {
+	$max = 100;
+	$min = 1;
+}
+
+$number = mt_rand($min,$max);
+$a = 1;
+
+fwrite(STDOUT, "Pick a number. Any number. Well, between $min and $max.");
 $guess = fgets(STDIN);
 
 
 do {
-	if ($guess < $number){
+	if ($guess == $number){
+		fwrite(STDOUT, "By Jove I think it's got it!");
+	} elseif ($guess < $number){
 		fwrite(STDOUT, "HIGHER WART! \n");
+		echo $number;
 		$guess = fgets(STDIN);
 		$a++;
 	}else {
 		fwrite(STDOUT, "LOWER, YOU FOOL!\n");
+		echo $number;
 		$guess = fgets(STDIN);
 		$a++;
 	}
 } while ($guess != $number);
 
 fwrite(STDOUT, "That'll do pig. That'll do. \nOnly $a tries.");
+
 
 ?>
