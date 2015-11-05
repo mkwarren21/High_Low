be8e717350987dda8569faeaa3a0b3d49e6da921
Mary Warren <Friday@Marys-MacBook-Air.local>
2015-11-04 23:21:25 -0600

    added mt_rand and guess counter

--- highlow.php
+++ highlow.php
@@ -1,20 +1,24 @@
 <?php
 
-$number = rand(1,100);
+$number = mt_rand(1,100);
+$a = 0;
 
 fwrite(STDOUT, 'Pick a number. Any number. Well, between 1 and 100.');
 $guess = fgets(STDIN);
+
 
 do {
 	if ($guess < $number){
 		fwrite(STDOUT, "HIGHER WART! \n");
 		$guess = fgets(STDIN);
+		$a++;
 	}else {
 		fwrite(STDOUT, "LOWER, YOU FOOL!\n");
 		$guess = fgets(STDIN);
+		$a++;
 	}
 } while ($guess != $number);
 
-fwrite(STDOUT, "That'll do pig. That'll do.");
+fwrite(STDOUT, "That'll do pig. That'll do. \nOnly $a tries.");
 
 ?>
